6d6e7b4804fa449cb460b3cce7ab4dbe7aeed202
Marcio Kleber Junior Lopes de Souza <msouza@tre-pa.jus.br>
2026-02-08 15:02:40 -0300

    feat: altera aplicação para funcionar na system tray

--- main.js
+++ main.js
@@ -1,33 +1,53 @@
-const { app, BrowserWindow } = require('electron');
+const { app, Tray, Menu, dialog } = require('electron');
 const path = require('path');
 
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 768,
-    title: 'GP Connect',
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,
-      nodeIntegration: false
-    }
-  });
+let tray = null;
 
-  mainWindow.loadFile('index.html');
+function createTray() {
+  tray = new Tray(path.join(__dirname, 'assets', 'tray-icon.png'));
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Connect',
+      click: () => {
+        // TODO: implementar conexão
+      }
+    },
+    {
+      label: 'Logout',
+      click: () => {
+        // TODO: implementar logout
+      }
+    },
+    { type: 'separator' },
+    {
+      label: 'About',
+      click: () => {
+        dialog.showMessageBox({
+          type: 'info',
+          title: 'Sobre',
+          message: 'GP Connect',
+          detail: 'Versão 1.0.0'
+        });
+      }
+    },
+    { type: 'separator' },
+    {
+      label: 'Sair',
+      click: () => {
+        app.quit();
+      }
+    }
+  ]);
+
+  tray.setToolTip('GP Connect');
+  tray.setContextMenu(contextMenu);
 }
 
 app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+  createTray();
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', (e) => {
+  e.preventDefault();
 });
